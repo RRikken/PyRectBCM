0ec6bf52684da394285ab31f9cf7be7461717a03
KReef <koen.reef@gmail.com>
2018-08-07 17:30:06 +0200

    docs

--- docs/conf.py
+++ docs/conf.py
@@ -65,7 +65,7 @@ except Exception as e:
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
 extensions = ['sphinx.ext.autodoc', 'sphinx.ext.intersphinx', 'sphinx.ext.todo',
               'sphinx.ext.autosummary', 'sphinx.ext.viewcode', 'sphinx.ext.coverage',
-              'sphinx.ext.doctest', 'sphinx.ext.ifconfig', 'sphinx.ext.imgmath',
+              'sphinx.ext.doctest', 'sphinx.ext.ifconfig', 'sphinx.ext.mathjax',
               'sphinx.ext.napoleon']
 
 # Napoleon Options
@@ -76,14 +76,14 @@ napoleon_google_docstring = True
 templates_path = ['_templates']
 
 # Mathjax path
-mathjax_path = 'https://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AMS-MML_HTMLorMML'
-mathjax_config = {
-    'extensions': ['tex2jax.js', 'AMSmath.js', 'AMSsymbols.js'],
-    'jax': ['input/tex', 'output/HTML-CSS'],
-    'TeX': {
-        'extensions: ["AMSmath.js", "AMSsymbols.js"]'
-    },
-}
+# mathjax_path = 'https://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AMS-MML_HTMLorMML'
+# mathjax_config = {
+#     'extensions': ['tex2jax.js', 'AMSmath.js', 'AMSsymbols.js'],
+#     'jax': ['input/tex', 'output/HTML-CSS'],
+#     'TeX': {
+#         'extensions: ["AMSmath.js", "AMSsymbols.js"]'
+#     },
+# }
 # The suffix of source filenames.
 source_suffix = '.rst'
 
@@ -185,7 +185,7 @@ else:
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = [] #'_static'
+html_static_path = ['_static'] #
 
 # If not '', a 'Last updated on:' timestamp is inserted at every page bottom,
 # using the given strftime format.
