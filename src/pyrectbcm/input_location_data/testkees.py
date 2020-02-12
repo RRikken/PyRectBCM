2261086a92155e10aff832ee66321b5308648d7b
robrikken <r.rikken@student.utwente.nl>
2020-02-12 22:33:33 +0100

    Changed model input data to a dictionary.

---
+++ src/pyrectbcm/input_location_data/testkees.py
@@ -0,0 +1,21 @@
+from numpy import pi
+
+test_kees = {
+    "name": "testkees",
+    "tidal_range": 1,
+    "tidal_frequency": 2 * pi / 44712,
+    "basin_width": 20e3,
+    "basin_length": 10e3,
+    "basin_depth": 2,
+    "ocean_depth": 30,
+    "wave_number": 8.2e-6 * 0,
+    "inlet_length": 1e3,
+    "inlet_depth": 2,
+    "inlet_width": 0.4e3,
+    "inlet_shape_factor": 0.005,
+    "number_of_inlets": 3,
+    "equilibrium_velocity": 1,
+    "sediment_import": 1e5,
+    "time_step": 0.5,
+    "end": 2,
+}
---
+++ src/pyrectbcm/input_location_data/testlocation.py
@@ -0,0 +1,21 @@
+from numpy import pi
+
+test_location = {
+    "name": "test_location",
+    "tide_amplitude": 1,
+    "tide_frequency": 2 * pi / 44712,
+    "basin_width": 50e3,
+    "basin_length": 5e3,
+    "basin_depth": 2,
+    "ocean_depth": 30,
+    "wave_number": 8.2e-6,
+    "inlet_length": 1e3,
+    "inlet_depth": 2,
+    "inlet_width": 0.4e3,
+    "inlet_shape": 0.005,
+    "number_of_inlets": 40,
+    "ueq": 1,
+    "sediment_import": 1e5,
+    "time_step": 0.5,
+    "end": 500,
+}
